28723cbfa3ad78b27d0e2927e0710e792b908d52
Erwan Guyomarch <erwan.guyomarch@synalogy.ch>
2019-07-05 20:39:03 +0200

    Starter update

--- index.js
+++ index.js
@@ -50,3 +50,186 @@ export {
     Modal,
     Sidemenu
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const state = {
+    user: {},
+    lists: {
+        items: []
+    }
+};
+
+const getters = {
+    user(state) { return state.user; },
+};
+
+const mutations = {
+    SET_USER(state, pack) {
+        state.user = pack.user;
+    },
+    SET_LISTS(state, pack) {
+        state.lists = pack.lists;
+    },
+    ADD_ITEM(state, pack) {
+        state.lists.items = pack;
+    },
+    REPLACE_ITEM(state, newItems) {
+        // newItems is array containing objects having the attribute 'id' to merge with state.lists.items
+        state.lists.items = state.lists.items.map(x => newItems.find(({ id }) => id === x.id) || x);
+    }
+};
+
+
+const actions = {
+    loadUser(context) {
+        fetchApi('/api/user').then(
+            (response) => { context.commit('SET_USER', response); },
+            (error) => { console.error('User init fail', error) }
+        );
+    },
+    loadLists({ getters, commit }) {
+        fetchApi('/api/lists').then(
+            (response) => { commit('SET_LISTS', response); },
+            (error) => { commit.error('User init fail', error) }
+        );
+    },
+    post(context, resource) {
+        return new Promise((resolve, reject) => {
+            fetchApi('/api/' + resource.target, {
+                method: 'post',
+                body: JSON.stringify(resource)
+            }).then(
+                (response) => {
+                    if(!response.exception) {
+                        // do an operation based on the target
+                        switch(resource.target) {
+                            case "item":
+                                context.commit('ADD_ITEM', response);
+                                break;
+                        }
+                        resolve(response)
+                    }
+                    else {
+                        reject(response)
+                    }
+                },
+                (error) => { reject(error) }
+            );
+        });
+    },
+    // special post function for the files
+    postFormData(context, args) {
+        return new Promise((resolve, reject) => {
+            fetch('/api/' + args.target, {
+                credentials: 'same-origin',
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+                    'Accept': 'application/json',
+                    //'Content-Type': 'multipart/form-data'
+                },
+                method: 'post',
+                body: args.formData,
+            }).then(
+                (r) => {
+                    r.json().then(
+                        (response) => {
+                            if(!response.exception) {
+                                // context.commit('addFromFormData', response);
+                                resolve(response)
+                            }
+                            else {
+                                reject(response)
+                            }
+                        },
+                        (error) => { reject(error); }
+                    );
+                },
+                (error) => { reject(error) }
+            );
+        });
+
+    },
+    put(context, resource) {
+        return new Promise((resolve, reject) => {
+            let options = {
+                method: 'put',
+                body: JSON.stringify(resource)
+            };
+            let path = '/api/' + resource.target + '/';
+            if(resource.id)
+                path += resource.id;
+            fetchApi(path, options).then(
+                (response) => {
+                    if(!response.exception) {
+                        // do an operation based on the target
+                        response.target = resource.target;
+                        response.id = resource.id;
+                        switch(resource.target) {
+                            case "item":
+                                context.commit('REPLACE_ITEM', response);
+                                break;
+                        }
+                        resolve(response)
+                    }
+                    else {
+                        reject(response)
+                    }
+                },
+                (error) => { reject(error) }
+            );
+        });
+    },
+    delete(context, resource) {
+        return new Promise((resolve, reject) => {
+            let options = { method: 'delete' };
+            let path = '/api/' + resource.target + '/';
+            if(resource.id)
+                path += resource.id;
+            else
+                options.body = JSON.stringify(resource);
+            fetchApi(path, options).then(
+                (response) => {
+                    if(!response.exception) {
+                        // do an operation based on the target
+                        response.target = resource.target;
+                        response.id = resource.id;
+                        switch(resource.target) {
+                            case "item":
+                                context.commit('DELETE_ITEM', response);
+                                break;
+                        }
+                        resolve(response)
+                    }
+                    else {
+                        reject(response)
+                    }
+                    response.exception ? reject(response) : resolve(response)
+                },
+                (error) => { reject(error) }
+            );
+        });
+    }
+};
+
+const store = new Vuex.Store({
+    strict: true,
+    state,
+    getters,
+    mutations,
+    actions
+});
+
+export default store;
